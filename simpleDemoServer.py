70824e11e67e03de54e96641d9754c4da84604c3
Barak Eitan <barak.ha.meleh@gmail.com>
2023-03-29 13:57:12 +0300

    Add files with timestamp and add timestamp to the json
    Add files with timestamp and add timestamp to json

--- simpleDemoServer.py
+++ simpleDemoServer.py
@@ -2,8 +2,9 @@ import http.server
 import socketserver
 import json
 import os
+import sys
 
-PORT = 8000  # Choose a port number you like
+PORT = 8001  # Choose a port number you like
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
 
@@ -26,39 +27,51 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 
         # Set the response headers
         self.send_header('Content-type', 'application/json')
+        self.send_header("Access-Control-Allow-Origin", "http://localhost:3000")
+        self.send_header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
         self.end_headers()
 
         cpu = disk = memory = ''
-        with open("files/cpu.txt", "rb") as file:
+        ts_cpu = ts_disk = ts_memory = '' # ts - TimeStamp
+        with open(sys.path[0] + "/files/cpu.txt", "rb") as file:
             try:
                 file.seek(-2, os.SEEK_END)
                 while file.read(1) != b'\n':
                     file.seek(-2, os.SEEK_CUR)
             except OSError:
                  file.seek(0)
-            cpu = (str)(file.readline()).split(' ')[-2]
+            line = (str)(file.readline()).split(' ')
+            cpu = line[-2]
+            ts_cpu = line[0]
+            
  
-        with open("files/disk.txt", "rb") as file:
+        with open(sys.path[0] + "/files/disk.txt", "rb") as file:
              try:
                  file.seek(-2, os.SEEK_END)
                  while file.read(1) != b'\n':
                      file.seek(-2, os.SEEK_CUR)
              except OSError:
                  file.seek(0)
-             disk = (str)(file.readline()).split(' ')[-2]
+             line = (str)(file.readline()).split(' ')
+             disk = line[-2]
+             ts_disk = line[0]
  
-        with open("files/memory.txt", "rb") as file:
+        with open(sys.path[0] + "/files/memory.txt", "rb") as file:
              try:
                  file.seek(-2, os.SEEK_END)
                  while file.read(1) != b'\n':
                      file.seek(-2, os.SEEK_CUR)
              except OSError:
                  file.seek(0)
-             memory = (str)(file.readline()).split(' ')[-2]
+             line = (str)(file.readline()).split(' ')
+             memory = line[-2]
+             ts_memory = line[0]
          
-        response = {'cpu': f'{cpu}', 'disk' : f'{disk}', 'memory':f'{memory}'}
+        response = {'cpu': f'{cpu}', 'disk' : f'{disk}', 'memory':f'{memory}', ts_cpu:f'{ts_cpu}'
+                    , ts_disk:f'{ts_disk}', ts_memory:f'{ts_memory}'}
         response_json = json.dumps(response).encode('utf-8')
         self.wfile.write(response_json)
+        
 
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
